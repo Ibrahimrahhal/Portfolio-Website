a6837d1276e07947aeaa6d92b7376dff5befcc61
ibrahim rahhal <ibrahim.rahhal3636@gmail.com>
2019-01-21 07:49:23 -0800

    icon fixes

--- gulpfile.js
+++ gulpfile.js
@@ -4,11 +4,13 @@ var autoprefixer = require('gulp-autoprefixer');
 var cleanCSS = require('gulp-clean-css');
 var autoprefixBrowsers = ['> 1%', 'last 2 versions', 'firefox >= 4', 'safari 7', 'safari 8', 'IE 8', 'IE 9', 'IE 10', 'IE 11'];
 var rename = require("gulp-rename");
+var sourcemaps = require('gulp-sourcemaps');
 
 
 
 gulp.task('default', () => {
   gulp.src(__dirname + "/dest/dest.css")
+    .pipe(sourcemaps.init())
     .pipe(importCss({
       skipComments: false
     }))
@@ -22,7 +24,7 @@ gulp.task('default', () => {
     //   console.log(`${details.name}: ${details.stats.originalSize}`);
     //   console.log(`${details.name}: ${details.stats.minifiedSize}`);
     // }))
-
+    .pipe(sourcemaps.write())
     .pipe(gulp.dest(__dirname + "/"));
 
 
@@ -31,6 +33,7 @@ gulp.task('default', () => {
 
   console.log("hi");
   gulp.src(__dirname + "/dest/contact.css")
+    .pipe(sourcemaps.init())
     .pipe(importCss({
       skipComments: false
     }))
@@ -44,12 +47,13 @@ gulp.task('default', () => {
     //   console.log(`${details.name}: ${details.stats.originalSize}`);
     //   console.log(`${details.name}: ${details.stats.minifiedSize}`);
     // }))
-
+    .pipe(sourcemaps.write())
     .pipe(gulp.dest(__dirname + "/"));
   console.log("hi");
 
 
   gulp.src(__dirname + "/Projects/css.css")
+    .pipe(sourcemaps.init())
     .pipe(importCss({
       skipComments: false
     }))
@@ -68,5 +72,6 @@ gulp.task('default', () => {
     //   console.log(`${details.name}: ${details.stats.minifiedSize}`);
     // }))
 
+    .pipe(sourcemaps.write())
     .pipe(gulp.dest("./Projects"));
 });
