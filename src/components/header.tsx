6f7154f154972b669d4dbbf2d14b3261c8147cae
Ibrahim Rahhal <ibrahim.rahhal3636@gmail.com>
2021-04-10 01:19:33 +0300

    issue-11: Refactor Hero Area (#12)
    * issue-11: Refactor Hero Area
    
    * issue-11: Refactor Hero Area
    
    * issue-11: Refactor Hero Area
    
    * issue-11: Refactor Hero Area
    
    Co-authored-by: Ibrahim Rahhal <ibrahim@unicode.ai>

---
+++ src/components/header.tsx
@@ -0,0 +1,48 @@
+import React, { ReactElement } from "react";
+import "./header.scss";
+
+export default (): ReactElement => {
+  return (
+    <header className="header">
+      <nav className="header__nav">
+        <div className="header__logo">
+          Ibrahim Rahhal
+          <div>
+            <div />
+          </div>
+        </div>
+        <div className="nav-menu">
+          <div className="nav-menu-list">
+            <a href="#skill" className="nav-menu-list__item">
+              Skills Set
+            </a>
+            <a href="#education" className="nav-menu-list__item">
+              Education
+            </a>
+            <a href="#projects" className="nav-menu-list__item">
+              Projects
+            </a>
+            <a
+              href="./Resume.pdf"
+              target="_blank"
+              className="nav-menu-list__item"
+            >
+              Resume
+            </a>
+            <a
+              href="https://github.com/Ibrahimrahhal"
+              className="nav-menu-list__item "
+              target="_blank"
+              rel="noreferrer"
+            >
+              <div className="github-icon" />
+            </a>
+          </div>
+        </div>
+      </nav>
+      <a className="header__contact" href="./form_handle.php?type=message">
+        Get in Touch
+      </a>
+    </header>
+  );
+};
