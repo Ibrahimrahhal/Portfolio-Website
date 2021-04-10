bf0ff3646051a4072839893401637b0dcba1ddd5
Ibrahim Rahhal <ibrahim@unicode.ai>
2021-04-10 08:20:40 +0300

    issue-22: Refactor Scrollbars

---
+++ src/components/hero/hero.tsx
@@ -0,0 +1,26 @@
+import React, { ReactElement } from "react";
+import "./hero.scss";
+
+export default (): ReactElement => {
+  return (
+    <div className="hero">
+      <div className="hero__circle" />
+      <h1 className="hero__title--primary">
+        Designer,<span> Fullstack Developer and Student</span>
+      </h1>
+      <h3 className="hero__title--secondary">Welcome to My Website</h3>
+      <div className="hero__hr" />
+      <div className="hero__call-to-action">
+        <h2 className="hero__call-to-action__title">
+          Have an <span>Intersting</span> Project
+        </h2>
+        <a
+          className="hero__call-to-action__btn"
+          href="./form_handle.php?type=prject"
+        >
+          Start a Project
+        </a>
+      </div>
+    </div>
+  );
+};
--- src/screens/home/home.tsx
+++ src/screens/home/home.tsx
@@ -1,10 +1,12 @@
 import React, { ReactElement } from "react";
-import Header from "../../components/header";
+import Header from "../../components/header/header";
+import Hero from "../../components/hero/hero";
 
 export default (): ReactElement => {
   return (
     <>
       <Header />
+      <Hero />
     </>
   );
 };
