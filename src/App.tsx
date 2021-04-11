737ca45cec6c741e2356032b5b620b39b16c98d3
Ibrahim Rahhal <ibrahim@unicode.ai>
2021-04-11 04:32:25 +0300

    issue-24: Setup Deployment with Github

--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,16 @@
-import React from "react";
+import React, { ReactElement } from "react";
 import Particles from "react-particles-js";
 import RootRouter from "./routers";
 import ParticleConfig from "./particle.config.json";
 import "./base.scss";
 
-function App() {
+function App(): ReactElement {
   return (
     <>
       <div className="site-wrapper">
         <Particles
           className="site-wrapper__background particles-js"
-          params={ParticleConfig as any}
+          params={ParticleConfig as never}
         />
         <div className="site-wrapper__content">
           <RootRouter />
