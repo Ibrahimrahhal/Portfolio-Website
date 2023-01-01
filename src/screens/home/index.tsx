80fb4a90ecb89ed60ab24b0c4726eeeb67ddff7d
Ibrahim Rahhal <ibrahim.rahhal3636@gmail.com>
2023-01-01 15:31:13 +0300

    adding get-in-touch

--- src/screens/home/index.tsx
+++ src/screens/home/index.tsx
@@ -5,8 +5,9 @@ import "@screens/home/index.scss";
 import Skills from "@components/skills";
 import Portfolio from "@components/portfolio";
 import OpenSource from "@components/open-source";
+import getInTouch from "@components/get-in-touch";
 
-const sections = [Hero, Skills, Portfolio, OpenSource];
+const sections = [Hero, Skills, Portfolio, OpenSource, getInTouch];
 export default (): ReactElement => {
   return (
     <>
