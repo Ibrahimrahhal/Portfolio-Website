4a0d057ac84d590d24aeb4c5235c52968a985696
Ibrahim Rahhal <ibrahim.rahhal3636@gmail.com>
2023-01-01 15:31:34 +0300

    adding get-in-touch

--- src/components/get-in-touch/index.tsx
+++ src/components/get-in-touch/index.tsx
@@ -2,25 +2,32 @@ import "@components/get-in-touch/index.scss";
 import Animation from "@components/animation";
 
 const animationOptions = {
-    loop: true,
-    autoplay: true,
-  };
+  loop: true,
+  autoplay: true,
+};
 export default () => {
   return (
     <div className="get-in-touch">
       <h1 className="get-in-touch__title">Get in Touch</h1>
       <div className="get-in-touch__animation-wrapper">
-      <Animation options={animationOptions} animation="friends" />
+        <Animation options={animationOptions} animation="friends" />
       </div>
       <p className="get-in-touch__desc">
-       I'm always excited to connect with new people and make some friendships. If you'd like to get in touch, please don't hesitate to send me an email or book a slot in my calender. I'm looking forward to getting to know you!
+        I'm always excited to connect with new people and make some friendships.
+        If you'd like to get in touch, please don't hesitate to send me an email
+        or book a slot in my calender. I'm looking forward to getting to know
+        you!
       </p>
       <div className="get-in-touch__actions">
-      <a target="_blank" href="https://calendar.app.google/MRkkEtRtGCHHhqY99" className="btn btn__calender">
-            Book a Slot
+        <a
+          target="_blank"
+          href="https://calendar.app.google/MRkkEtRtGCHHhqY99"
+          className="btn btn__calender"
+        >
+          Book a Slot
         </a>
         <a href="mailto:ibrahim.rahhal3636@gmail.com" className="btn">
-           Send an Email 
+          Send an Email
         </a>
       </div>
     </div>
